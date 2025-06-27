1f7d1a991b2f09e3219110f6c171fa3f5747f0b7
Pierre Alan <obou291@gmail.com>
2025-06-27 08:59:48 +0100

    Fix Vercel path error serving static files

--- api/index.js
+++ api/index.js
@@ -2,24 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Servir les fichiers statiques du dossier public
-app.use(express.static(path.join(__dirname, '../public')));
+// Détecter le répertoire racine en production Vercel
+const publicPath = path.resolve(__dirname, '../public');
+
+// Servir les fichiers statiques
+app.use(express.static(publicPath));
 
 // Pour toute autre route, renvoyer index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-module.exports = app;const express = require('express');
-const app = express();
-
-// Servir les fichiers statiques du dossier public
-app.use(express.static('public'));
-
-// Route principale
-app.get('/', (req, res) => {
-  res.send('Bienvenue sur mon site Node.js + Express depuis Vercel !');
-});
-
-// Export de l'application pour Vercel
 module.exports = app;

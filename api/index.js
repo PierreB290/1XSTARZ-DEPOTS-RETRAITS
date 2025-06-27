957f1fb0d4fa96db82c2c4b966d2248bd9b011b8
Pierre Alan <obou291@gmail.com>
2025-06-27 08:52:23 +0100

    Configure Express to serve static files

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 const express = require('express');
+const path = require('path');
+const app = express();
+
+// Servir les fichiers statiques du dossier public
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Pour toute autre route, renvoyer index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = app;const express = require('express');
 const app = express();
 
 // Servir les fichiers statiques du dossier public

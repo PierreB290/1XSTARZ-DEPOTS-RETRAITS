9cd618385fe481188bc4d37cf565411df8c403f3
Pierre Alan <obou291@gmail.com>
2025-06-27 09:16:03 +0100

    Fix: Corrige le fichier API Express (suppression doublons)

--- api/index.js
+++ api/index.js
@@ -13,20 +13,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-// Export pour Vercel
-module.exports = app;const express = require('express');
-const path = require('path');
-const app = express();
-
-// Détecter le répertoire racine en production Vercel
-const publicPath = path.resolve(__dirname, '../public');
-
-// Servir les fichiers statiques
-app.use(express.static(publicPath));
-
-// Pour toute autre route, renvoyer index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
-
+// Exporter pour Vercel
 module.exports = app;
